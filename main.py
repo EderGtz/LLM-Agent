78ff1fb7da9918db49ebaeac2dc479db5a4f5b61
Eder Gutierrez <gutierrezeder64@gmail.com>
2025-12-02 19:10:44 -0600

    FEAT: Implement CLI argument parsing using argparse
    - Replaced hardcoded test prompt with dynamic command-line input.
    - Configured ArgumentParser to accept a required positional 'prompt' argument.
    - Integrated parsed arguments directly into the Gemini API client call.

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
-#Reading the API key and connecting to Gemini
+#read the key-value pairs from the .env file and set them in the environment
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-
-#Response from the AI returns an object. If I want to see the
-#answer I should use the .text property
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=contents
-)
 
 def main():
-    print(f"This is the content:\n{contents}\nThis is the response:\n")
-    print(response.parts)
+
+    #Reading the API key and connecting to Gemini
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    #Module for analyzing an user input at the cmd
+    parser = argparse.ArgumentParser(
+        description = "Send the text to Gemini AI") #What the program does
+    parser.add_argument("prompt",type=str,help="The user prompt")
+    args = parser.parse_args()
+
+    #Response from the AI returns an object. If I want to see the
+    #answer I should use the .text property
+    contents = args.prompt
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=contents
+    )
+
+    #Keeping track of tokens usage
+    if response.usage_metadata == None:
+        raise RuntimeError("API call request failed")
+
+    print(f"""User prompt: {contents}
+    Prompt tokens: {response.usage_metadata.prompt_token_count}
+    Response tokens: {response.usage_metadata.candidates_token_count}
+    Response:
+    {response.text}
+""")
 
 
 if __name__ == "__main__":
     main()
-
