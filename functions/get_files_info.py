c1d1fb3d6f211c7a4bf13cc4bb5683b4abcf7166
Eder Gutierrez <gutierrezeder64@gmail.com>
2025-12-03 15:45:49 -0600

    FIX: Correct accumulation logic and return flow in get_files_info tool

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,10 +5,11 @@ import os
 def get_files_info(working_directory, directory="."):
     #directory es treated as a relative path within wd
 
+    contents_of_directory = ""
     if directory == ".":
-        print("Result for current directory:")
+        contents_of_directory += "Result for current directory:\n"
     else:
-        print(f"Result for '{directory}' directory:")
+        contents_of_directory += f"Result for '{directory}' directory:\n"
 
     #If the absolute path of directory is outside the wd, return Error
     #This is a guardrail, so the LLM cannot perform anything outside wd gave
@@ -16,11 +17,13 @@ def get_files_info(working_directory, directory="."):
     path_to_dir = os.path.abspath(os.path.join(working_directory, directory))
 
     if not path_to_dir.startswith(path_to_wd):
-        return f'   Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        contents_of_directory += f'   Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
+        return contents_of_directory
     
     #In case the directory argument is not a directory
     if not os.path.isdir(path_to_dir):
-        return  f'  Error: "{directory}" is not a directory'
+         contents_of_directory +=   f'  Error: "{directory}" is not a directory\n'
+         return contents_of_directory
     
     try:
         for file in os.listdir(path_to_dir):
@@ -28,9 +31,9 @@ def get_files_info(working_directory, directory="."):
             full_path = os.path.join(path_to_dir, file)
             size_of_file = os.path.getsize(full_path)
             is_dir = os.path.isdir(full_path)
-
-            print(f"    - {file}: file_size={size_of_file} bytes, is_dir={is_dir}")
-        #Returns a str representing the contents of a directory
+            contents_of_directory += f"    - {file}: file_size={size_of_file} bytes, is_dir={is_dir}\n"
+        return contents_of_directory
+            #Returns a str representing the contents of a directory
 
     except Exception as e:
         return f"Error: {e}"
